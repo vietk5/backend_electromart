79964bec705d170cef6ec214049bf23addda9c58
vietk5 <148680923+vietk5@users.noreply.github.com>
2026-01-08 07:49:33 +0700

    fix

---
+++ src/main/java/com/electromart/backend/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.electromart.backend.repository;
+
+import com.electromart.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
